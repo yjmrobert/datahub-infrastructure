cc9f22eb39ed7ea80bb821d23dddd928bc9cecd4
Yan <yjmrobert@gmail.com>
2022-07-04 12:51:54 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# DataHub Infrastructure
+
+
+## Getting Started
+
+### Create New Environment
+
+1. Run the /env/state with your variables to launch the remote state container for your environment's terraform states
---
+++ terraform/templates/state/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "${var.environment}-tfstate"
+  location = "Canada Central"
+
+  tags = {
+    environment = "${var.environment}"
+    project = "datahub state"
+  }
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.resource_code.result}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "blob"
+}
