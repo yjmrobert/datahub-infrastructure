40fedee0d74bd2e3c241e334faba2a3ea669387b
Yan <yjmrobert@gmail.com>
2022-07-20 01:42:10 -0400

    updated write output to write information for event handling in csharp

--- scripts/utils/output_test.ps1
+++ scripts/utils/output_test.ps1
@@ -1,16 +1,16 @@
 
 
-Write-Output "[Fake] Running terraform init";
+Write-Information "[Fake] Running terraform init";
 Start-Sleep -Seconds 1.5
 
-Write-Output "[Fake] Running terraform plan";
+Write-Information "[Fake] Running terraform plan";
 Start-Sleep -Seconds 1.5
 
-Write-Output "[Fake] Plan saved at apply-plan.out";
+Write-Information "[Fake] Plan saved at apply-plan.out";
 Start-Sleep -Seconds 1.5
 
-Write-Output "[Fake] Running terraform apply";
+Write-Information "[Fake] Running terraform apply";
 Start-Sleep -Seconds 1.5
 
-Write-Output "[Fake] Successfully applied terraform plan";
+Write-Information "[Fake] Successfully applied terraform plan";
         
