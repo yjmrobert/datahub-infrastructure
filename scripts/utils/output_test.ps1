8fc6e9336a8d9df7d222c540f4a78b3ce85409f5
Yan <yjmrobert@gmail.com>
2022-07-20 15:38:04 -0400

    shortened output test

--- scripts/utils/output_test.ps1
+++ scripts/utils/output_test.ps1
@@ -1,16 +1,16 @@
 
 
 Write-Information "[Fake] Running terraform init";
-Start-Sleep -Seconds 1.5
+Start-Sleep -Seconds 0.5
 
 Write-Information "[Fake] Running terraform plan";
-Start-Sleep -Seconds 1.5
+Start-Sleep -Seconds 0.75
 
 Write-Information "[Fake] Plan saved at apply-plan.out";
-Start-Sleep -Seconds 1.5
+Start-Sleep -Seconds 0.25
 
 Write-Information "[Fake] Running terraform apply";
-Start-Sleep -Seconds 1.5
+Start-Sleep -Seconds 1
 
 Write-Information "[Fake] Successfully applied terraform plan";
         
